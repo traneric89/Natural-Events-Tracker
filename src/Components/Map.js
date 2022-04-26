266651a9cb6cbf4006b1218ab050108760196316
optiX <55358375+traneric89@users.noreply.github.com>
2022-04-26 19:26:09 -0400

    Location markers added

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "./Marker";
+import MarkerCard from "./MarkerCard";
 
-const Map = ({ center, zoom }) => {
+const Map = ({ center, zoom, data }) => {
   const defaultProps = {
     center: {
-      lat: 42.3265,
-      lng: -122.8756,
+      lat: 45.3265,
+      lng: -100.8756,
     },
     zoom: 5,
   };
+
+  const [locationInfo, setLocationInfo] = useState(null);
+
+  const markers = data.map((event) => {
+    if (event.categories[0].id === 8) {
+      return (
+        <Marker
+          lat={event.geometries[0].coordinates[1]}
+          lng={event.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: event.id, title: event.title })}
+        />
+      );
+    }
+  });
 
   return (
     <div className="map">
@@ -18,8 +34,9 @@ const Map = ({ center, zoom }) => {
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <Marker lat={defaultProps.center.lat} lng={defaultProps.center.lng} />
+        {markers}
       </GoogleMapReact>
+      {locationInfo ? <MarkerCard info={locationInfo} /> : null}
     </div>
   );
 };
